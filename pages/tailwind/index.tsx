83ff78a8a5dc2bb62fff19e3e355dda8b2b6edee
kiko-g <kikojpgoncalves@gmail.com>
2024-01-27 20:44:56 +0000

    add: resizeable divs

--- pages/tailwind/index.tsx
+++ pages/tailwind/index.tsx
@@ -3,8 +3,15 @@ import clsx from "clsx";
 import { Layout } from "@/components/layout";
 
 export default function TailwindDemo() {
-  const sizes = ["xs", "sm", "md", "lg", "xl", "full"];
-  const [size, setSize] = React.useState("md");
+  const sizes: Record<string, string> = {
+    xs: "350px",
+    sm: "576px",
+    md: "768px",
+    lg: "1024px",
+    full: "100%",
+  };
+
+  const [selectedSize, setSelectedSize] = React.useState<keyof typeof sizes>("full");
 
   return (
     <Layout title="Tailwind">
@@ -12,13 +19,13 @@ export default function TailwindDemo() {
         <div className="flex items-center justify-between rounded border bg-white p-3 dark:border-transparent dark:bg-black/30">
           <div></div>
           <div className="flex items-center gap-2">
-            {sizes.map((item, itemIdx) => (
+            {Object.keys(sizes).map((item) => (
               <button
                 key={item}
-                onClick={() => setSize(item)}
+                onClick={() => setSelectedSize(item)}
                 className={clsx(
                   "rounded px-2 py-1 text-xs text-gray-500 transition dark:bg-gray-800 dark:text-gray-400",
-                  item === size
+                  item === selectedSize
                     ? "bg-cyan-500 text-white dark:bg-cyan-600 dark:text-white"
                     : "hover:bg-cyan-500/40 dark:hover:bg-gray-700",
                 )}
@@ -28,7 +35,12 @@ export default function TailwindDemo() {
             ))}
           </div>
         </div>
-        <div className="flex h-full flex-col rounded border bg-white dark:border-transparent dark:bg-black/30"></div>
+        <div
+          className="flex h-full flex-col rounded border bg-white transition-all dark:border-transparent dark:bg-black/30"
+          style={{
+            width: sizes[selectedSize],
+          }}
+        ></div>
       </div>
     </Layout>
   );
